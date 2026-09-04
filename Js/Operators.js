9b3f802e4d2449afa7142ff3b6d9bfb3e98b15cf
Be2CNO <sudosuprv@gmail.com>
2026-09-04 17:36:21 +0530

    Understanding Different Types of Operators

---
+++ Js/Operators.js
@@ -0,0 +1,71 @@
+// Arithmetic Operators
+
+let a = 32;
+let b = 87;
+let c = 74;
+let d = 2;
+let e = 65;
+
+console.log("Addition");
+
+console.log(a + b);
+console.log(b + b);
+console.log(e + a);
+console.log(b + d);
+console.log(a + b);
+console.log(e + c);
+
+console.log("Subtraction");
+
+console.log(a - b);
+console.log(e - b);
+console.log(d - c);
+console.log(a - d);
+console.log(c - b);
+
+console.log("Multiplication")
+
+console.log(a * b);
+console.log(b * d);
+console.log(e * b);
+console.log(a * c);
+console.log(e * b);
+console.log(c * d);
+
+console.log("Division");
+
+console.log(a/e)
+console.log(c/a)
+console.log(b/d)
+console.log(e/c)
+console.log(d/a)
+
+console.log("Modular");
+
+console.log(a%e)
+console.log(c%a)
+console.log(b%d)
+console.log(e%c)
+console.log(d%a)
+
+console.log("Exponents")
+
+console.log(a**e)
+console.log(b**d)
+console.log(c**e)
+console.log(a**c)
+console.log(b**c)
+
+// Unary Operators
+
+let x = 76;
+x = x + 1;
+x += 1;
+console.log(x++)
+console.log(x)
+
+//     - Pre
+
+let h = 3;
+console.log(++h)
+console.log(h++)
