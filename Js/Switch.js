2cd48e82898374d8ae2f148233827dbd220c8d9a
Be2CNO <sudosuprv@gmail.com>
2026-09-05 23:45:55 +0530

    Understanding Switch in JS

---
+++ Js/Switch.js
@@ -0,0 +1,61 @@
+console.log('Understanding Switches in Js')
+
+let color = "Black"
+
+switch (color){
+    case "Black":{
+        console.log("Just Do it Black")
+    }
+    break;
+    case "Red" :{
+        console.log("Just Do it Red")
+    }
+    break;
+    case "Pink" : {
+        console.log("Just do it pink")
+    }
+    break;
+    default:{
+        console.log("Fuck You!!!")
+    }
+}
+
+
+// Practice Question
+
+let day = 3;
+
+switch (day){
+    case 1:{
+        console.log('Today is Monday')
+    }
+    break;
+    case 2:{
+        console.log('Today is Tuesday')
+    }
+    break;
+    case 3:{
+        console.log('Today is Wednesday')
+    }
+    break;
+    case 4:{
+        console.log('Today is Thursday')
+    }
+    break;
+    case 5:{
+        console.log('Today is Friday')
+    }
+    break;
+    case 6:{
+        console.log('Today is Saturday')
+    }
+    break;
+    case 7:{
+        console.log('Today is Sunday')
+    }
+    break;
+    default :{
+        console.log("Loop h bhai")
+    }
+}
+
