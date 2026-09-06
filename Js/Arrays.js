96b3061a430486589d8995aed90f05a15ada1138
Be2CNO <sudosuprv@gmail.com>
2026-09-06 19:20:32 +0530

    Understanding Arrays

--- Js/Arrays.js
+++ Js/Arrays.js
@@ -1,2 +1,39 @@
 let ary = ["Nmy","Nmta","Hagda","tgda"]
+console.log(ary) 
+
+ary[0] = "A";
 console.log(ary)
+
+console.log(ary.length)
+ary[19] = "Le le re beta selfy"
+console.log(ary)
+
+// Arrays Methods 
+
+var ary1 = ['car1','car2','car3','car4']
+ary1.push("Car45")  // add in last 
+console.log(ary1)
+
+console.log(ary1.pop());    // delete last element and returns it
+console.log(ary1) 
+
+console.log(ary1) 
+ary1.unshift("Car0")
+console.log(ary1) 
+
+
+console.log(ary1) 
+ary1.shift()
+console.log(ary1) 
+
+
+let days = ["day1","day2","day3","day4","day5","day6","day7"]
+
+console.log(days.indexOf("day4"))
+console.log(days.indexOf("day2"))
+console.log(days.indexOf("day5"))
+
+console.log(days.includes("day5"))
+console.log(days.includes("day2"))
+console.log(days.includes("day"))
+
